00f8302c3efff50a60abdaae10923ce1f089df91
DESKTOP-J1OP3L4\ez220405 <orca610@naver.com>
2022-08-17 18:26:13 +0900

    0817 mysql 변환전 테스트

--- pro30_t/src/main/java/com/myspring/pro30_t/board/service/BoardService.java
+++ pro30_t/src/main/java/com/myspring/pro30_t/board/service/BoardService.java
@@ -4,14 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 import com.myspring.pro30_t.board.vo.ArticleVO;
-import com.myspring.pro30_t.board.vo.ImageVO;
 
 public interface BoardService {
-//	public List<ArticleVO> listArticles() throws Exception;
-	public Map listArticles(Map pagingMap) throws Exception;
+	public List<ArticleVO> listArticles() throws Exception;
 	public int addNewArticle(Map articleMap) throws Exception;
 	public Map viewArticle(int articleNO) throws Exception;
+	//public Map viewArticle(int articleNO) throws Exception;
 	public void modArticle(Map articleMap) throws Exception;
 	public void removeArticle(int articleNO) throws Exception;
-	public void removeModImage(ImageVO imageVO) throws Exception;
 }
